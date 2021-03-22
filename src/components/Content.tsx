1bce556b3f4d18b5200a86b61bce30e4f5d90626
Saul <saulbento@gmail.com>
2021-03-22 14:33:02 +0100

    first commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,45 @@
+import { ReactNode } from 'react';
+
+import { MovieCard } from './MovieCard';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  genres: Array<GenreResponseProps>;
+  selectedGenre: GenreResponseProps;
+  movies: Array<MovieProps>;
+}
+
+export function Content({ genres, selectedGenre, movies }: ContentProps ) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
